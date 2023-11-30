a935d31b983fd7a4ed201afe75684f4067608194
unknown <65301282014@utc.ac.th>
2023-11-30 20:44:39 +0700

    Online_shop_System

---
+++ database/seeders/BlogSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Blog;
+class BlogSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Blog::factory()->count(20)->create();
+    }
+}
